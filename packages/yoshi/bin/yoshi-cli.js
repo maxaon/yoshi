689f4d267bd34bc3308af69c4eef458f49a2e956
Ronen Amiel <ronen.amiel@gmail.com>
2019-11-23 22:02:21 +0200

    add telemetry

--- packages/yoshi/bin/yoshi-cli.js
+++ packages/yoshi/bin/yoshi-cli.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
 const config = require('yoshi-config');
+const { inTeamCity } = require('yoshi-helpers/queries');
+const { collectData } = require('yoshi-common/build/telemetry');
+
+if (inTeamCity()) {
+  collectData();
+}
 
 if (config.projectType === 'app') {
   require('yoshi-flow-app/build/bin/yoshi-app');
